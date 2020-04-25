6f163c5eed701b2ef8341ca5abbb7d2f52fbda02
Niels Dequeker <niels.dequeker@fastmail.com>
2020-04-25 16:03:45 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 import daemon
+import logging
 
 from controlSonos import do_main_program
 
-with daemon.DaemonContext():
-    do_main_program()
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler("./output.log")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+with daemon.DaemonContext(files_preserve = [handler.stream]):
+    do_main_program(logger)
