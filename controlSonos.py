7f5482eb08acd16d87457a0be20654ac49b5b8f1
Niels Dequeker <niels.dequeker@fastmail.com>
2020-04-25 13:05:09 +0200

    Update controlSonos.py

--- controlSonos.py
+++ controlSonos.py
@@ -26,39 +26,40 @@ def evaluateResponse(response):
         result = u'\u2713'.encode('utf8')
     return result
 
-print "Script started. Searching for " + buttonname + "..."
-try:
-    while True:
-            devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
-            if len(devices) == 0:
-                print("No devices found, try running with sudo")
-                sys.exit(1)
-
-            for device in devices:
-                if device.name == buttonname:
-                    print("Button found: " + str(device))
-                    url = "http://" + host + ":" + port + "/"+ roomname
-                    try:
-                        device.grab()
-                        for event in device.read_loop():
-                            if event.type == evdev.ecodes.EV_KEY:
-                                 data = evdev.categorize(event)  # Save the event temporarily to introspect it
-                                 if data.keystate == 1:  # Down events only
-                                    if data.keycode in key2commandPairs:
-                                        print "Received event " + data.keycode + " -> Sending command " + key2commandPairs[data.keycode],
-                                        response = urllib2.urlopen(url + "/" + key2commandPairs[data.keycode]).read()
-                                        print  evaluateResponse(response)
-                                    else:
-                                        print u'You Pressed the {} key!'.format(data.keycode)  # Print it all out!
-                    except Exception, e:
-                        print e
-                        print("Lost connection! Start searching...")
-                        pass
-                else:
-                    pass
-except KeyboardInterrupt:
-    print 'Exited'
+def do_main_program():
+    print "Script started. Searching for " + buttonname + "..."
     try:
-        sys.exit(1)
-    except SystemExit:
-        os._exit(0)
+        while True:
+                devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+                if len(devices) == 0:
+                    print("No devices found, try running with sudo")
+                    sys.exit(1)
+
+                for device in devices:
+                    if device.name == buttonname:
+                        print("Button found: " + str(device))
+                        url = "http://" + host + ":" + port + "/"+ roomname
+                        try:
+                            device.grab()
+                            for event in device.read_loop():
+                                if event.type == evdev.ecodes.EV_KEY:
+                                     data = evdev.categorize(event)  # Save the event temporarily to introspect it
+                                     if data.keystate == 1:  # Down events only
+                                        if data.keycode in key2commandPairs:
+                                            print "Received event " + data.keycode + " -> Sending command " + key2commandPairs[data.keycode],
+                                            response = urllib2.urlopen(url + "/" + key2commandPairs[data.keycode]).read()
+                                            print  evaluateResponse(response)
+                                        else:
+                                            print u'You Pressed the {} key!'.format(data.keycode)  # Print it all out!
+                        except Exception, e:
+                            print e
+                            print("Lost connection! Start searching...")
+                            pass
+                    else:
+                        pass
+    except KeyboardInterrupt:
+        print 'Exited'
+        try:
+            sys.exit(1)
+        except SystemExit:
+            os._exit(0)
